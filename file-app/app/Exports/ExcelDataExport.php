7377c5d1ec4d21262173fe970050999c2af415fa
github <github>
2023-06-18 09:50:55 +0900

    Excelファイルのエクスポート時にヘッダーをつける

--- file-app/app/Exports/ExcelDataExport.php
+++ file-app/app/Exports/ExcelDataExport.php
@@ -4,15 +4,25 @@ namespace App\Exports;
 
 use App\Models\ExcelData;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExcelDataExport implements FromCollection
+class ExcelDataExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        // return ExcelData::all();
         return ExcelData::all()->makeHidden(['id', 'created_at', 'updated_at']);
+    }
+
+    //出力エクセルにヘッダー追加
+    public function headings():array
+    {
+        return [
+            '名前',
+            '年齢',
+            '誕生日'
+        ];
     }
 }
